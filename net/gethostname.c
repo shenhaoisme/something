188fa722910cdf13fab9daf86a1aef2340474054
shenhaoisme <shenhaoisme@163.com>
2018-10-14 22:38:55 +0800

    Update gethostname.c

--- net/gethostname.c
+++ net/gethostname.c
@@ -27,6 +27,15 @@ char **h_addr_list;	  //主机的IP地址列表
  *
  *
  * */
+
+/*
+#include <arpe/inet.h>
+int inet_pton(int family, const char *strptr, void *addrptr);     //将点分十进制的ip地址转化为用于网络传输的数值格式
+        返回值：若成功则为1，若输入不是有效的表达式则为0，若出错则为-1
+ 
+const char * inet_ntop(int family, const void *addrptr, char *strptr, size_t len);     //将数值格式转化为点分十进制的ip地址格式
+        返回值：若成功则为指向结构的指针，若出错则为NULL
+*/
 #include <stdio.h>
 
 #include <arpa/inet.h>
